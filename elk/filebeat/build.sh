1e9731146865857dbb7f2c5e1b676e584ec2895f
Jack <python@sohu.com>
2018-04-16 06:13:51 -0700

    add tls key/crt example

--- elk/filebeat/build.sh
+++ elk/filebeat/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cp filebeat.yml /opt/product/data/elk
+cp /etc/pki/tls/openssl.cnf ./
+cp /etc/pki/tls/certs/logstash-forwarder.crt ./
+cp /etc/pki/tls/private/logstash-forwarder.key ./
 docker build -t filebeat:v6.2.3 ./
-
